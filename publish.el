a68ed02686ff0440a907ab353710bdda668eeab4
Elis Hirwing <elis@hirwing.se>
2023-12-28 10:20:09 +0100

    feat(site): Add open graph tags

--- publish.el
+++ publish.el
@@ -26,6 +26,16 @@
          :html-preamble "<div id=\"logo\"><img src=\"/logo.png\" alt=\"Alt text\"></div>"
 	     :html-head-extra "
            <meta name=\"referrer\" content=\"no-referrer\" />\n<link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>
+
+           <!-- Open graph tags -->
+           <meta property=\"og:description\" content=\"Fördomsfri mötesplats för veganer och vegeterianer att prata om etiska värderingar, receptbyten, träning, hälsa med mera.\" />
+           <meta property=\"og:image\" content=\"https://via.elis.nu/og-image.png\" />
+           <meta property=\"og:locale\" content=\"sv\" />
+           <meta property=\"og:site_name\" content=\"Vegan i Arvika\" />
+           <meta property=\"og:title\" content=\"Vegan i Arvika - Startsida\" />
+           <meta property=\"og:type\" content=\"website\" />
+           <meta property=\"og:url\" content=\"https://via.elis.nu/\" />
+           <meta name=\"twitter:card\" content=\"summary_large_image\" />
          "
          :html-container "article"            ; Set HTML container
          :html-doctype "html5"                ; Make it html5
@@ -45,6 +55,13 @@
          :html-preamble "<div id=\"logo\"><img src=\"/logo.png\" alt=\"Alt text\"></div><div><a href=\"/\">⬅️ Tillbaka till startsidan</a></div>"
 	     :html-head-extra "
            <meta name=\"referrer\" content=\"no-referrer\" />\n<link rel=\"stylesheet\" href=\"/style.css\" type=\"text/css\"/>
+
+           <!-- Open graph tags -->
+           <meta property=\"og:image\" content=\"https://via.elis.nu/og-image.png\" />
+           <meta property=\"og:locale\" content=\"sv\" />
+           <meta property=\"og:site_name\" content=\"Vegan i Arvika\" />
+           <meta property=\"og:type\" content=\"website\" />
+           <meta name=\"twitter:card\" content=\"summary_large_image\" />
          "
          :html-container "article"            ; Set HTML container
          :html-doctype "html5"                ; Make it html5
@@ -61,7 +78,7 @@
          :publishing-directory "./output"
          :publishing-function org-publish-attachment
          :exclude ".*"
-         :include [ "logo.png" "style.css" "CNAME" ])
+         :include [ "og-image.png" "logo.png" "style.css" "CNAME" ])
 
         ("flyer"
          :base-directory "./src"
